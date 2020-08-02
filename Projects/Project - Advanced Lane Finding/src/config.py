d072938c7ecda2c375da1ee8888421b826f0e00c
Seymur Dadashov <seymur@ualberta.ca>
2020-08-02 02:59:52 -0600

    main project py

---
+++ Projects/Project - Advanced Lane Finding/src/config.py
@@ -0,0 +1,6 @@
+# Define global variables
+global left_fit_global     # Global vector for polyfit params
+global right_fit_global    # Global vector for polyfit params
+global falseCount          # Counter to see if complete histogram search is required
+global videoType           # Different settings are used depending if project, challenger, or harder challenge
+global curveDist           # Distance between curves
